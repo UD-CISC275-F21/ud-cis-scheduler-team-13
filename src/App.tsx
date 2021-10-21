403ff868ab8096bf36e3c24acbe4f6ffbfd3cfc1
Sebastian Cortes <scortesz@udel.edu>
2021-10-21 11:16:31 -0400

    Clean test code

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 // import {Container, Row, Col} from "react-bootstrap";
 
@@ -8,24 +7,7 @@ import "./App.css";
 function App(): JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                UD CIS Scheduler
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                    Dennis Mirza
-                </p>
-                <p>
-                    Tim Longoria
-                </p>
-                <p> Sebastian Cortes</p>
-            </header>
-                    
-            {/* <Container>
-                <Row>
-                    <Col>Bootstrap column</Col>
-                </Row>
-            </Container>     */}
+       
         </div>
     );
 }
