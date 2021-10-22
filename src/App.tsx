50def117ccd06fd7e7c39a9d2fba08ac8cdb9b6d
timblakel <timblake@udel.edu>
2021-10-22 13:47:17 -0400

    added all semester table

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import { AllSemestersTable } from "./components/AllSemestersTable";
 function App(): JSX.Element {
     return (
         <div className="App">
-<<<<<<< HEAD
             <div className="top-bar">
 
                 
@@ -16,10 +15,7 @@ function App(): JSX.Element {
             </div>
 
             <p> Welcome! To start creating your schedule, click on the boxes below.</p>
-=======
             <AllSemestersTable></AllSemestersTable>
-            UD CIS Scheduler
->>>>>>> b714d022bd3e03a5ffda4c2924b013d7180cd50e
         </div>
     );
 }
