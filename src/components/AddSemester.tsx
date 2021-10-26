32480a97cd0178d154ea2bdb202e647f63bcfebb
timblakel <timblake@udel.edu>
2021-10-26 19:33:39 -0400

    added season select, year form, and submit button

--- src/components/AddSemester.tsx
+++ src/components/AddSemester.tsx
@@ -1,17 +1,39 @@
-import { useState } from "react";
-import React, { Container, Dropdown, DropdownButton, Form } from "react-bootstrap";
+import { ChangeEvent, SyntheticEvent, useState } from "react";
+import React, { Container, Row, Dropdown, DropdownButton, Form, Col, Button } from "react-bootstrap";
 
 export function AddSemester(): JSX.Element {
     const [dropdownSeason, setDropdownSeason] = useState<string>("Fall");
+    const [year, setYear] = useState<number>(2022);
 
-    return <Container>
-        <Form>
-            <DropdownButton id="dropdown-season" title={dropdownSeason}>
-                <Dropdown.Item>Fall</Dropdown.Item>
-                <Dropdown.Item>Winter</Dropdown.Item>
-                <Dropdown.Item>Spring</Dropdown.Item>
-                <Dropdown.Item>Summer</Dropdown.Item>
+    function handleSeasonSelect(key: string | null, event: SyntheticEvent<unknown>): void {
+        if (key !== null) {
+            setDropdownSeason(key);
+        }
+    }
+
+    function yearChange(e: ChangeEvent<HTMLInputElement>): void {
+        const year: number = parseInt(e.target.value,10);
+        setYear(year);
+    }
+
+    return <Row>
+        <Col>
+            <DropdownButton id="dropdown-season" title={dropdownSeason} onSelect={handleSeasonSelect}>
+                <Dropdown.Item eventKey="Fall">Fall</Dropdown.Item>
+                <Dropdown.Item eventKey="Winter">Winter</Dropdown.Item>
+                <Dropdown.Item eventKey="Spring">Spring</Dropdown.Item>
+                <Dropdown.Item eventKey="Summer">Summer</Dropdown.Item>
             </DropdownButton>
-        </Form>
-    </Container>;
+        </Col>
+        <Col>
+            <Form>
+                <Form.Group>
+                    <Form.Control type="year" placeholder="year" onChange={yearChange}></Form.Control>
+                </Form.Group>
+            </Form>
+        </Col>
+        <Col>
+            <Button type="submit">Submit</Button>
+        </Col>
+    </Row>;
 }
