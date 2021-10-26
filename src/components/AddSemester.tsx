044a1d31ee82b960d6192a70f00551e0312d5f40
timblakel <timblake@udel.edu>
2021-10-26 18:58:14 -0400

    added 4 seasons to dropdown

--- src/components/AddSemester.tsx
+++ src/components/AddSemester.tsx
@@ -1,4 +1,17 @@
-import { Container } from "react-bootstrap";
+import { useState } from "react";
+import React, { Container, Dropdown, DropdownButton, Form } from "react-bootstrap";
 
 export function AddSemester(): JSX.Element {
-    return <Container></Container>;}
+    const [dropdownSeason, setDropdownSeason] = useState<string>("Fall");
+
+    return <Container>
+        <Form>
+            <DropdownButton id="dropdown-season" title={dropdownSeason}>
+                <Dropdown.Item>Fall</Dropdown.Item>
+                <Dropdown.Item>Winter</Dropdown.Item>
+                <Dropdown.Item>Spring</Dropdown.Item>
+                <Dropdown.Item>Summer</Dropdown.Item>
+            </DropdownButton>
+        </Form>
+    </Container>;
+}
