828a24f0fb0c5246c30aa6b70c17bb611683a34b
timblakel <timblake@udel.edu>
2021-10-26 02:05:47 -0400

    added const courses

--- src/components/AllSemestersTable.tsx
+++ src/components/AllSemestersTable.tsx
@@ -1,7 +1,12 @@
 import { SemesterTable } from "./SemesterTable";
 import React, { Container, Row, Col } from "react-bootstrap";
+import { Course } from "../interfaces/Course";
 
 export function AllSemestersTable(): JSX.Element {
+    const courses: Course[] = [
+        {id: "CISC275", name: "Introduction to Software Engineering", description: "Description 1", prereqs: ["CISC220"]},
+        {id: "CISC181", name: "Introduction to Computer Science II", description: "Description 2", prereqs: ["CISC108"]},
+        {id: "CISC210", name: "Introduction to Systems Programming", description: "Description 3", prereqs: ["CISC108"]}];
 
     return <Container>
         <Row>
