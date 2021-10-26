7c78f20f144fa62aa9d58a78790e65596e47dc91
timblakel <timblake@udel.edu>
2021-10-26 02:05:24 -0400

    removed const courses

--- src/components/SemesterTable.tsx
+++ src/components/SemesterTable.tsx
@@ -2,10 +2,6 @@ import { Course } from "../interfaces/Course";
 import React, { Container, Table } from "react-bootstrap";
 
 export function SemesterTable({semesterName, courses}: {semesterName: string, courses: Course[]}): JSX.Element {
-    // const courses: Course[] = [
-    //     {id: "CISC275", name: "Introduction to Software Engineering", description: "Description 1", prereqs: ["CISC220"]},
-    //     {id: "CISC181", name: "Introduction to Computer Science II", description: "Description 2", prereqs: ["CISC108"]},
-    //     {id: "CISC210", name: "Introduction to Systems Programming", description: "Description 3", prereqs: ["CISC108"]}];
     
     // Different semesters have different credit limits
     // Uncomment lines when use for creditLim is implemented
