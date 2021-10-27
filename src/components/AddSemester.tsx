3aa28fda700f847891b402f5b97f548aaf16c17d
timblakel <timblake@udel.edu>
2021-10-26 20:02:31 -0400

    header

--- src/components/AddSemester.tsx
+++ src/components/AddSemester.tsx
@@ -26,24 +26,29 @@ export function AddSemester({allCourses, setAllCourses}: {
         setAllCourses(newAllCourses);
     }
 
-    return <Row>
-        <Col>
-            <DropdownButton id="dropdown-season" title={dropdownSeason} onSelect={handleSeasonSelect}>
-                <Dropdown.Item eventKey="Fall">Fall</Dropdown.Item>
-                <Dropdown.Item eventKey="Winter">Winter</Dropdown.Item>
-                <Dropdown.Item eventKey="Spring">Spring</Dropdown.Item>
-                <Dropdown.Item eventKey="Summer">Summer</Dropdown.Item>
-            </DropdownButton>
-        </Col>
-        <Col>
-            <Form>
-                <Form.Group>
-                    <Form.Control type="year" placeholder="year" onChange={yearChange}></Form.Control>
-                </Form.Group>
-            </Form>
-        </Col>
-        <Col>
-            <Button type="submit" onClick={addSemester}>Submit</Button>
-        </Col>
-    </Row>;
+    return <Container>
+        <Row>
+            <h3>Add Semester to Plan</h3>
+        </Row>
+        <Row>
+            <Col>
+                <DropdownButton id="dropdown-season" title={dropdownSeason} onSelect={handleSeasonSelect}>
+                    <Dropdown.Item eventKey="Fall">Fall</Dropdown.Item>
+                    <Dropdown.Item eventKey="Winter">Winter</Dropdown.Item>
+                    <Dropdown.Item eventKey="Spring">Spring</Dropdown.Item>
+                    <Dropdown.Item eventKey="Summer">Summer</Dropdown.Item>
+                </DropdownButton>
+            </Col>
+            <Col>
+                <Form>
+                    <Form.Group>
+                        <Form.Control type="year" placeholder="year" onChange={yearChange}></Form.Control>
+                    </Form.Group>
+                </Form>
+            </Col>
+            <Col>
+                <Button type="submit" onClick={addSemester}>Submit</Button>
+            </Col>
+        </Row>
+    </Container>;
 }
