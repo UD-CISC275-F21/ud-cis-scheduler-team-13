b7f99c3ef5256644416d554aa1b76a3b0e657574
Sebastian Cortes <scortesz@udel.edu>
2021-10-27 17:30:30 -0400

    Initial implementation of header

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import "./App.css";
 import { AllSemestersTable } from "./components/AllSemestersTable";
+import { Header } from "./components/Header";
 
 
 function App(): JSX.Element {
     return (
         <div className="App">
-            <div className="top-bar">
 
-                
-                <h1> UD CIS Scheduler</h1>
-                
-            </div>
-
-            <p> Welcome! To start creating your schedule, click on the boxes below.</p>
+            <Header></Header>
             <AllSemestersTable></AllSemestersTable>
+
         </div>
     );
 }
