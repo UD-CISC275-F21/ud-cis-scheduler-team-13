a004c831ecab5a3b316d7937dfae4372fe08ea71
Sebastian Cortes <scortesz@udel.edu>
2021-10-27 22:41:53 -0400

    Change Container into div for header to look better

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,28 @@
 import React from "react";
-import { Container } from "react-bootstrap";
 import Logo from "../img/logo-udel.png";
 
 export function Header(): JSX.Element {
     return(
-        <Container>
-            <Container className="udHeader">
-                <Container className="udHeaderContainer">
-                    <Container className="udHeaderWrapper">
+        <div>
+            <div className="udHeader">
+                <div className="udHeaderContainer">
+                    <div className="udHeaderWrapper">
                         <a href="https://www.udel.edu/" className="udHeaderLogo">
                             <img src={Logo} alt="University of Delaware" className="img-responsive"/>
                         </a>
-                    </Container>
-                </Container>
-            </Container>
+                    </div>
+                </div>
+            </div>
 
-            <Container className="udHeader-collegeOuterWrapper">
-                <Container className="udHeader-collegeBar">
-                    <Container className="udHeader-collegeWrapper">
-                        <Container className="udHeader-collegeName">
+            <div className="udHeader-collegeOuterWrapper">
+                <div className="udHeader-collegeBar">
+                    <div className="udHeader-collegeWrapper">
+                        <div className="udHeader-collegeName">
                             <a href="#"> UD CIS Scheduler</a>
-                        </Container>
-                    </Container>
-                </Container>
-            </Container>
-        </Container>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
