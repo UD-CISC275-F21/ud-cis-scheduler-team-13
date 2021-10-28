9f785975025409b481d5f1950358afd349653083
Sebastian Cortes <scortesz@udel.edu>
2021-10-27 21:29:57 -0400

    Added second bar with website title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,27 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import Logo from "../img/logo-udel.png";
 
 export function Header(): JSX.Element {
     return(
-        <Container className="udHeader">
-            <Container className="udHeaderContainer">
-                <Container className="udHeaderWrapper">
-                    <a href="https://www.udel.edu/" className="udHeaderLogo">
-                        <img src="../assets/logo-udel.png" alt="University of Delaware" className="img-responsive"/>
-                    </a>
+        <Container>
+            <Container className="udHeader">
+                <Container className="udHeaderContainer">
+                    <Container className="udHeaderWrapper">
+                        <a href="https://www.udel.edu/" className="udHeaderLogo">
+                            <img src={Logo} alt="University of Delaware" className="img-responsive"/>
+                        </a>
+                    </Container>
+                </Container>
+            </Container>
+
+            <Container className="udHeader-collegeOuterWrapper">
+                <Container className="udHeader-collegeBar">
+                    <Container className="udHeader-collegeWrapper">
+                        <Container className="udHeader-collegeName">
+                            <a href="#"> UD CIS Scheduler</a>
+                        </Container>
+                    </Container>
                 </Container>
             </Container>
         </Container>
