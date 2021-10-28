fe470322d1b53402970dfbe19d9ab875aa049ba4
Sebastian Cortes <scortesz@udel.edu>
2021-10-27 21:37:35 -0400

    Initial Commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+export function Header(): JSX.Element {
+    return(
+        <Container>
+            Footer goes here.
+        </Container>
+    );
+}
