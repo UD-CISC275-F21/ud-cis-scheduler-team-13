45976516f454338b2d747b5ab822a3f7c5a6cdb9
timblakel <timblake@udel.edu>
2021-10-29 15:53:43 -0400

    removed imports

--- src/components/AddSemester.tsx
+++ src/components/AddSemester.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import React, { Container, Row, Dropdown, DropdownButton, Form, Col, Button } from "react-bootstrap";
+import React, { Container, Row, Dropdown, DropdownButton, Button } from "react-bootstrap";
 import { Course } from "../interfaces/Course";
 
 export function AddSemester({allCourses, setAllCourses}: {
