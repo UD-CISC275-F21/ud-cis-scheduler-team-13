6c9729eb1611f296557229017554aceae82d53b7
Sebastian Cortes <you@example.com>
2021-11-01 18:31:02 -0400

    Removed placeholder # with website's link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,7 +18,7 @@ export function Header(): JSX.Element {
                 <div className="udHeader-collegeBar">
                     <div className="udHeader-collegeWrapper">
                         <div className="udHeader-collegeName">
-                            <a href="#"> UD CIS Scheduler</a>
+                            <a href="https://ud-cisc275-f21.github.io/ud-cis-scheduler-team-13/"> UD CIS Scheduler</a>
                         </div>
                     </div>
                 </div>
