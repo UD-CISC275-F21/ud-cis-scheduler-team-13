c261a590952a00d52cf2b36a3983d12a55d04c99
Sebastian Cortes <you@example.com>
2021-11-01 22:06:57 -0400

    Removed unused imports

--- src/components/Switch.tsx
+++ src/components/Switch.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 import Scheduler from "./ShowTables";
 import Home from "./Home";
